503b9c14fde0d50f049747b8ad628f98d7fade1c
Thomas Marquart <thomas@marquart.se>
2025-12-01 09:32:17 +0100

    fix

--- vald_web/settings.py
+++ vald_web/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = ['vald.astro.uu.se','localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    "vald",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "vald",
 ]
 
 MIDDLEWARE = [
